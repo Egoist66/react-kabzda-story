7146b56a98fde9be89df2816e767006232cd1070
Farid Mahmudov <razormad666@gmail.com>
2023-09-21 17:08:25 +0300

    init

---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,154 @@
+import {ChangeEvent, FocusEvent, useRef, useState} from "react";
+import {Button} from "../components/Button";
+import {action} from '@storybook/addon-actions'
+
+export const UncontrolledInput = () => <input placeholder={'enter text'} type="text"/>
+export const ControlledFixedInputWithDefault = () => <input defaultValue={'Hello'} placeholder={'enter text'} type="text"/>
+export const ControlledFixedInputWithHardCode = () => <input value={'Hello'} placeholder={'enter text'} type="text"/>
+export const UnControlledInputWithState = () => {
+
+    const [text, setText] = useState<string>('')
+
+    return (
+        <>
+
+            <input
+                value={text}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => setText(e.currentTarget.value)}
+                placeholder={'enter text'}
+                type="text"
+            />
+
+            <div>{text}</div>
+
+        </>
+    )
+}
+
+export const ControlledInputWithRef = () => {
+
+    const [value, setValue] = useState<string>('')
+    const ref = useRef<HTMLInputElement>(null)
+
+    const getValue = () => {
+        if(ref.current){
+            setValue(ref.current.value)
+            ref.current.value = ''
+
+        }
+    }
+
+    return (
+        <>
+
+            <input ref={ref} placeholder={'enter text'} type="text"
+            />
+
+            <Button text={'Get Value'} onClickHandler={getValue} />
+
+
+            <div>{value}</div>
+        </>
+    )
+}
+
+export const ControlledDoubleFlowInput = () => {
+    const [value, setValue] = useState<string>('')
+    const [isFocused_one, setFocusOne] = useState<boolean>(false)
+    const [isFocused_second, setFocusSecond] = useState<boolean>(false)
+
+    const onChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+    }
+
+
+    return (
+        <div>
+
+            <input id={'one'} value={value}
+                   onFocus={() => setFocusOne(true)}
+                   onBlur={() => setFocusOne(false)}
+                   onChange={onChangeValue}
+                   placeholder={isFocused_one ? 'Enter value' : 'OutValue'}
+                   type="text"
+            />
+
+            <input
+                id={'second'}
+                onFocus={() => setFocusSecond(true)}
+                onBlur={() => setFocusSecond(false)}
+                value={value}
+                onChange={onChangeValue}
+                placeholder={isFocused_second ? 'Enter value' : 'OutValue'}
+                type="text"
+            />
+
+        </div>
+    )
+}
+
+export const OutControlledInput = () => {
+
+    const [parentValue, setParentValue] = useState<string>('dfdf')
+
+
+    return (
+        <input value={parentValue} onChange={(e) => setParentValue(e.currentTarget.value)} type="text"/>
+    )
+}
+
+export const OutControlledCheckBox = () => {
+    const [parentValue, setParentValue] = useState<boolean>(true)
+
+
+    return (
+
+        <>
+
+            <input
+                checked={parentValue}
+                onChange={(e) => setParentValue(e.currentTarget.checked)}
+                type="checkbox"
+            />
+
+            <p>{parentValue.toString()}</p>
+
+        </>
+    )
+}
+export const ControlledSelect = () => {
+    const [value, setValue] = useState<string | undefined>(undefined)
+
+    return (
+        <>
+
+            <select
+                onChange={(e: ChangeEvent<HTMLSelectElement>) => setValue(e.currentTarget.value)}
+                value={value} name="select" >
+                <option>Выберите значение</option>
+                <option value="1">Minsk</option>
+                <option value="2">New York</option>
+                <option value="3">Vegas</option>
+            </select>
+
+            {value}
+
+        </>
+    )
+}
+
+
+
+
+
+
+
+export default {
+    title: 'Example/Inputs',
+    //component: UncontrolledInput,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+}
+
---
+++ src/stories/UncontrolledAccordion.stories.tsx
@@ -0,0 +1,83 @@
+import {FC, ReactElement, ReactNode, useState} from "react";
+import {Accordion, AccordionWithTotlaControl} from "../components/Accordion";
+
+
+type ListPropsType = {
+    data?: Array<{ name: string, id: string }>
+    render?: () => ReactNode | ReactElement
+}
+const AccordionBody: FC<ListPropsType> = ({data, render}) => {
+
+    return (
+        <ul>
+            {data ? data.map(d => (
+                <>
+                    <li  key={d.id}>{d.name}</li>
+
+                </>
+            )): render ? render(): <p>No data</p>}
+
+        </ul>
+    )
+}
+
+
+const list = [
+    {name: 'Farid', id: crypto.randomUUID()},
+    {name: 'Jack', id: crypto.randomUUID()},
+    {name: 'Peter', id: crypto.randomUUID()},
+]
+
+
+export const ControlledAccordion: FC = () => {
+
+    return (
+        <Accordion
+            isOpen={false}
+            body={(
+                <>
+                    <Accordion isOpen={true} body={<AccordionBody data={list}/>} title={'-- Users --'}/>
+                    <Accordion body={<AccordionBody render={() => <h2>Hello</h2>}/>} title={'-- Greeting --'}/>
+                    <Accordion body={<AccordionBody render={() => <img src="https://shorturl.at/duENT" alt="d"/>}  />} title={'-- Nature --'} />
+                    <Accordion body={<AccordionBody render={() => <img src="https://shorturl.at/duENT" alt="d"/>}  />} title={'-- Nature --'} />
+
+                </>
+            )}
+            title={'Show Info'}
+            trigger={(toggleAccordion, title) => <button onClick={toggleAccordion}>{title}</button>}
+        />
+    )
+}
+
+export const FullyControlledAccordion: FC = () => {
+    const [open, setOpen] = useState<boolean>(false)
+
+    const toggleAccordion = () => {
+        setOpen(open => !open)
+    }
+
+
+    return <AccordionWithTotlaControl
+        isOpen={open}
+        body={<AccordionBody data={list}/>}
+        trigger={(toggleAccordion, title) => <button onClick={toggleAccordion}>{title}</button>}
+        title={'-- AccordionWithTotlaControl --'}
+        onClickHandler={toggleAccordion}
+    />
+
+}
+
+export const UnControlledAccordion: FC = () => {
+    return (
+        <Accordion/>
+    )
+}
+
+
+export default {
+    title: 'Example/Accordion',
+    //component: ControlledAccordion,
+    tags: ['autodocs'],
+
+}
+
